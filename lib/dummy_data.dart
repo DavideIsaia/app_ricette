1bc120ce84ec4732928d66e83c8a5f69d552a5f0
DavideIsaia <davide.enjoy@hotmail.it>
2023-01-19 12:05:50 +0100

    first commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: unnecessary_const
+
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italiani',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Facile e veloce',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Tedeschi',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Lights',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Esotici',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Colazione',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asiatici',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Francesi',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Estivi',
+    color: Colors.teal,
+  ),
+];
