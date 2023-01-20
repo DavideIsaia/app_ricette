a8f27c9fd0b66fe4e51aae49a0ecfc50edf4ff0a
DavideIsaia <davide.enjoy@hotmail.it>
2023-01-20 12:15:02 +0100

    inserito un drawer a scorrimento

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,35 +2,55 @@ import 'package:app_ricette/screens/categories_screen.dart';
 import 'package:app_ricette/screens/favorites_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class TabsScreen extends StatefulWidget {
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categorie'},
+    {'page': FavoritesScreen(), 'title': 'Piatti preferiti'},
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        //opzionale, indica con quale tab aperta vuoi partire 0 per categories, 1 per preferiti
-        // initialIndex: 0, 
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Piatti'),
-            bottom: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorie',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Preferiti',
-              ),
-            ]),
+    // DefaultTabController(
+    //     length: 2,
+    //index opzionale, indica con quale tab aperta vuoi partire 0 per categories, 1 per preferiti
+    // initialIndex: 0,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white60,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorie',
           ),
-          body: TabBarView(
-            children: [CategoriesScreen(), FavoritesScreen()],
-          ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Preferiti',
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cuciniamo!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
